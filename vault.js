a6e82e75b917572f6b91323cd9fe4c46857cfea7
m440i <156387560+m440i@users.noreply.github.com>
2024-01-30 18:54:37 -0800

    Add files via upload

---
+++ vault.js
@@ -0,0 +1,14 @@
+// String with mando message
+const message =
+  "You have have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+// Assign three variables with result of each calculation
+// Each one must equal one of the 3 codes in combo
+// Combo of lock is 10 - 49 - 39
+let x = 5 + 5;
+let y = 7 * 7;
+let z = 40 - 1;
+
+// Display it on an html page or alert
+//alert(message + x "-" + y "-" z);
+
+alert(message + x + "-" + y + "-" + z);
